9b0d6ad812328aecfa0853515156c6d14e2372e7
Kan <kanalovefootball@gmail.com>
2024-07-10 14:45:19 +0700

    ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,8 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  for (let n = 0; n <= numbers.length; n++) {
+    for (let i = 0; i <= numbers.length; i++) {}
+  }
+}
+
+numbers = [2, 7, 11, 15];
+console.log(twoSum(numbers, 24));
