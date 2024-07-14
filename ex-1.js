d198d323386c1d76be47db90fbe554aae1f90700
Kan <kanalovefootball@gmail.com>
2024-07-14 14:42:06 +0700

    edit ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,18 @@
 function twoSum(numbers, target) {
-  for (let n = 0; n <= numbers.length; n++) {
-    for (let i = 0; i <= numbers.length; i++) {}
+  for (let n = 0; n < numbers.length; n++) {
+    for (let j = n + 1; j < numbers.length; j++) {
+      if (numbers[n] + numbers[j] === target) {
+        return [n, j];
+      }
+    }
   }
+  return [];
 }
 
 numbers = [2, 7, 11, 15];
+
 console.log(twoSum(numbers, 24));
+
+// numbers = [2, 7, 11, 15, 25, 30, 35];
+
+// console.log(twoSum(numbers, 55));
